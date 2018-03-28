5e4edd2c6a3db48f669bdf1f330cadb8cef9ce88
ckgurav <31270217+ckgurav@users.noreply.github.com>
2018-03-28 16:00:02 +0530

    Go programing practice

---
+++ 15_Slice/01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	mySlice:=make([]int,0,5)
+
+	fmt.Println(mySlice,len(mySlice),cap(mySlice))
+
+	for i:=0;i<100;i++{
+		mySlice=append(mySlice,i)
+		fmt.Println( len(mySlice),cap(mySlice))
+	}
+}
