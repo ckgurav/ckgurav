f15a4c54190c8479a13b9a24b0225569a4911e5c
ckgurav <31270217+ckgurav@users.noreply.github.com>
2018-03-30 09:55:08 +0530

    Go programing practice

--- 16_map/01/main.go
+++ 16_map/01/main.go
@@ -9,8 +9,8 @@ func main()  {
 	mapExample["a1"]=11
 	mapExample["a2"]=12
 fmt.Println(mapExample,len(mapExample))
-	for _,V1:=range (mapExample){
-		fmt.Println(V1)
+	for No,V1:=range (mapExample){
+		fmt.Println(No,V1)
 	}
 	//delete(mapExample,"a1")
 	i,j:=mapExample["a1"]
---
+++ 16_map/05/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main()  {
+
+	mySet:=map[int]bool{
+		1:true,
+		2:true,
+	}
+
+
+
+}
