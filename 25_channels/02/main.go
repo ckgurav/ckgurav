a989356ad06a7bad58324f8aa118e89423ac1d35
ckgurav <31270217+ckgurav@users.noreply.github.com>
2018-04-10 15:34:07 +0530

    Go programing practice

---
+++ 25_channels/02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	c:=make(chan int)
+
+	go func() {
+		for i:=0;i<20;i++{
+			c<-i
+			fmt.Println("inside:",i)
+
+		}
+		close(c)
+	}()
+
+	for n:=range c{
+		fmt.Println("Outside",n)
+	}
+}
